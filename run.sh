e80dc7cb8da4d8ecb151636916376ca30908b82d
Tamas Pasztor <tomcsi@tomcsi.hu>
2021-06-07 16:59:00 +0200

    add agent

--- run.sh
+++ run.sh
@@ -7,3 +7,5 @@ mkdir $MNT_POINT
 echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
 s3fs $S3_BUCKET $MNT_POINT -o passwd_file=passwd -o use_path_request_style -o endpoint=hu2-k3s -o url=$S3_URL
 
+/usr/bin/containerssh-agent wait-signal --signal INT --signal TERM
+
