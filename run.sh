94325f06d7ab1b0e2bda94bc765559537b545fe9
Tamas Pasztor <tomcsi@tomcsi.hu>
2021-06-07 16:54:45 +0200

    remove bash

--- run.sh
+++ run.sh
@@ -7,5 +7,3 @@ mkdir $MNT_POINT
 echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
 s3fs $S3_BUCKET $MNT_POINT -o passwd_file=passwd -o use_path_request_style -o endpoint=hu2-k3s -o url=$S3_URL
 
-/bin/bash
-
